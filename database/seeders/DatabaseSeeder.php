12b3931ea7463c52e4205a4e6217a71120547589
Oscar Sabogal <oscarsabogal285@gmail.com>
2023-09-12 19:27:02 -0500

    feat: firts commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Note;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        User::factory()->create([
+            'email' => 'oscarsabogal285+1@gmail.com',
+            'password' => bcrypt('123456')
+        ]);
+
+        Note::factory(30)->create();
+    }
+}
